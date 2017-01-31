bcafc332e73e8d1800720db3cb8c2972ad32801e
alina3agit <antonova.alina@gmail.com>
2017-01-31 15:20:17 +0100

    Step 2.1 upd 5

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include
 #from qa.views import test, question_list, question_detail, popular
 #from qa.views import question_ask, question_answer
 #from qa.views import user_signup, user_login, user_logout
 
-urlpatterns = [
-    url(r'^$', 'qa.views', name='question_list'),
-    url(r'^question/(?P<pk>\d+)/', 'qa.views', name='question_detail'),
-    url(r'^popular/', 'qa.views', name='popular'),
-    url(r'^ask/', 'qa.views', name='question_ask'),
+urlpatterns = patterns('qa.views',
+    url(r'^$', 'test'),
+    url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question_detail'),
+    url(r'^popular/.*', 'test', name='popular'),
+    url(r'^ask/.*', 'test', name='question_ask'),
     #url(r'^answer/', question_answer, name='question_answer'),
-    url(r'^signup/', 'qa.views', name='signup'),
-    url(r'^login/', 'qa.views', name='login'),
+    url(r'^signup/.*$', 'test', name='signup'),
+    url(r'^login/.*$', 'test', name='login'),
     #url(r'^logout/', user_logout, name='logout'),
-    url(r'^new/', 'qa.views', name='new'),
-]
+    url(r'^new/.*', 'test', name='new'),
+)

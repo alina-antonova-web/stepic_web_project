d7c51983a036957e71e573d23f08ae558e0910aa
alina3agit <antonova.alina@gmail.com>
2017-01-31 14:50:44 +0100

    Step 2.1 attempt 1

---
+++ ask/qa/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from qa.views import test, question_list, question_detail, popular
+from qa.views import question_ask, question_answer
+from qa.views import user_signup, user_login, user_logout
+
+urlpatterns = [
+    url(r'^$', question_list, name='question_list'),
+    url(r'^question/(?P<pk>\d+)/', question_detail, name='question_detail'),
+    url(r'^popular/', popular, name='popular'),
+    url(r'^ask/', question_ask, name='question_ask'),
+    #url(r'^answer/', question_answer, name='question_answer'),
+    url(r'^signup/', user_signup, name='signup'),
+    url(r'^login/', user_login, name='login'),
+    #url(r'^logout/', user_logout, name='logout'),
+    url(r'^new/', test, name='new'),
+]
--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn_wsgi.conf /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart

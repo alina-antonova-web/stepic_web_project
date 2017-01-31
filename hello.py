96a9038d75f1baecf090d7ca1a9ae2ca05bf359d
alina3agit <antonova.alina@gmail.com>
2017-01-31 11:17:51 +0100

    Step 1.9. attempt 2

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-def wsgi_application(environ, start_response):
+def application(environ, start_response):
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    body = bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")
+    body = str('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")
     start_response(status, headers)
     return [body]

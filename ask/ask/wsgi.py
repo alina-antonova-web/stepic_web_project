80d313a281671b89f0eb85a586317195a073b335
alina3agit <antonova.alina@gmail.com>
2017-02-01 22:22:49 +0100

    Step 2.1 upd 14

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn_wsgi.conf /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/qa.py
 sudo /etc/init.d/gunicorn restart
+gunicorn -c /home/box/web/etc/django.py wsgi

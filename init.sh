b657f39072b92468964d409ca6bc096a976885d5
alina3agit <antonova.alina@gmail.com>
2017-02-01 12:48:52 +0100

    Step 2.1 upd 9

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
-rm /etc/nginx/sites-enabled/default
-ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-/etc/init.d/nginx restart
-ln -sf /home/box/web/etc/gunicorn_wsgi.conf /etc/gunicorn.d/hello.py
-/etc/init.d/gunicorn restart
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn_wsgi.conf /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
 
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+sudo gunicorn -c /home/box/web/etc/django.py wsgi --daemon
